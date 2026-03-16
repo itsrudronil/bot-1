455699354b7406c9c7d39d70c0a09b8374ec7b40
itsrudronil <workforrudronil@gmail.com>
2026-03-17 00:02:33 +0600

    Update reconnect.js

--- reconnect.js
+++ reconnect.js
@@ -1,1 +1,10 @@
+const config = require('./config')
 
+function reconnect(startBot){
+  console.log(`🔄 Reconnecting in ${config.reconnectDelay/1000} seconds...`)
+  setTimeout(() => {
+    startBot()
+  }, config.reconnectDelay)
+}
+
+module.exports = reconnect
