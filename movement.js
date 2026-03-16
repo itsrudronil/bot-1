ce11f7413fc8f2179db8a0619833b22609902014
itsrudronil <workforrudronil@gmail.com>
2026-03-17 00:00:23 +0600

    Update movement.js

--- movement.js
+++ movement.js
@@ -1,1 +1,24 @@
+function start(bot) {
+  const actions = ['forward','back','left','right']
 
+  setInterval(() => {
+    const action = actions[Math.floor(Math.random()*actions.length)]
+    bot.setControlState(action,true)
+    bot.setControlState('jump',true)
+
+    // Random look around
+    bot.look(
+      Math.random() * Math.PI * 2,
+      Math.random() * Math.PI * 2,
+      true
+    )
+
+    setTimeout(() => {
+      bot.setControlState(action,false)
+      bot.setControlState('jump',false)
+    }, 2000)
+
+  }, 8000)
+}
+
+module.exports = { start }
