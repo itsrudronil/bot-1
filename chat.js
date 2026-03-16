189d237c810f93c7ad76e1034ea24a40fc0c9db6
itsrudronil <workforrudronil@gmail.com>
2026-03-17 00:01:52 +0600

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,1 +1,14 @@
+function startChat(bot){
+  const messages = [
+    "Welcome to Infinite SMP",
+    "I am Sinzo the gop gop master"
+    "Araf is Gay"
+]
 
+  setInterval(() => {
+    const msg = messages[Math.floor(Math.random() * messages.length)]
+    bot.chat(msg)
+  }, 20000) // every 20 seconds
+}
+
+module.exports = { startChat: startChat }
